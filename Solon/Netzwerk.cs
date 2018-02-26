4478f910e6bd6f5ba13b9a0e235ff579aca86470
jasmina mojdehkar <mojdehkar497j@htl-ottakring.ac.at>
2018-02-26 13:18:16 +0100

    Client Logik angefangen

---
+++ Solon/Netzwerk.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Solon
+{
+    class Netzwerk
+    {
+        static void Netzwerkkonfig()
+        {
+            var localEndpoint = new IPEndPoint(IPAddress.Loopback, 4040);
+            TcpClient client = new TcpClient(localEndpoint);
+
+            client.SendTimeout = 3000;
+            client.ReceiveTimeout = 3000;
+
+            var remoteEndpoint = new IPEndPoint(IPAddress.Loopback, 4242);
+            client.Connect(remoteEndpoint);
+        }
+    }
+}
